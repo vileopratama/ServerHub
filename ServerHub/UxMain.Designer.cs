8da43b0e9df6d524da304bf409439dfe8c97c7e3
suhe <hendarsyahss@gmail.com>
2017-03-05 23:58:52 +0800

    Update Hub

--- ServerHub/UxMain.Designer.cs
+++ ServerHub/UxMain.Designer.cs
@@ -266,6 +266,7 @@
             this.Controls.Add(this.Header);
             this.Controls.Add(this.bunifuGradientPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "UxMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Server Hub";
